5e810ec5018cf16aa8cfac2100791b07c6c98aca
DavidSantanaks <davidfjobs@gmail.com>
2023-05-27 10:42:43 -0300

    ADD todas pastas no git e configuração do banco

---
+++ src/main/java/com/suporte/ticket/sistemas/Ticket/de/suporte/TicketDeSuporteApplication.java
@@ -0,0 +1,13 @@
+package com.suporte.ticket.sistemas.Ticket.de.suporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDeSuporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketDeSuporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suporte/ticket/sistemas/Ticket/de/suporte/TicketDeSuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suporte.ticket.sistemas.Ticket.de.suporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDeSuporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
